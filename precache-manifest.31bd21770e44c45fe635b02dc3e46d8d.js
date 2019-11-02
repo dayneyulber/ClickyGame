31d305221646b61ba9617dabf8b2ba5cda2efb45
dayne beronque <dayneyulber@gmail.com>
2019-11-01 19:41:38 -0500

    Updates

---
+++ precache-manifest.31bd21770e44c45fe635b02dc3e46d8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb84cc8822732fb3df62e8c6d5b8499",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "9811d73356baa6f2ca3c",
+    "url": "/ClickyGame/static/css/main.21d6a827.chunk.css"
+  },
+  {
+    "revision": "5554d9c0912e2769b230",
+    "url": "/ClickyGame/static/js/2.cfdb9847.chunk.js"
+  },
+  {
+    "revision": "9811d73356baa6f2ca3c",
+    "url": "/ClickyGame/static/js/main.9faf5f02.chunk.js"
+  },
+  {
+    "revision": "eea99bef7721d0a18d16",
+    "url": "/ClickyGame/static/js/runtime-main.eda1b526.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.db69209b1ce9fc1064d71c938382b2bc.js"
+  "/ClickyGame/precache-manifest.31bd21770e44c45fe635b02dc3e46d8d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ClickyGame/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
