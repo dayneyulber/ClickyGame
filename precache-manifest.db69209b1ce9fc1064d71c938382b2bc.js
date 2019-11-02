86d6538acb645547e1634762893a67091ae60b8c
dayne beronque <dayneyulber@gmail.com>
2019-11-01 19:37:36 -0500

    Updates

---
+++ precache-manifest.db69209b1ce9fc1064d71c938382b2bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf228b4264cd1beafd95040c9936831",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "9811d73356baa6f2ca3c",
+    "url": "/react-gh-pages/static/css/main.21d6a827.chunk.css"
+  },
+  {
+    "revision": "5554d9c0912e2769b230",
+    "url": "/react-gh-pages/static/js/2.cfdb9847.chunk.js"
+  },
+  {
+    "revision": "9811d73356baa6f2ca3c",
+    "url": "/react-gh-pages/static/js/main.9faf5f02.chunk.js"
+  },
+  {
+    "revision": "3876e63ff2657c38a158",
+    "url": "/react-gh-pages/static/js/runtime-main.c6120ddc.js"
+  }
+]);
